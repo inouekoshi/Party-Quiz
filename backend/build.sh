e9bfda81157aaa179b457cdd35311be133f4fbeb
InoueKoshi <130729590+inouekoshi@users.noreply.github.com>
2026-04-30 14:38:39 +0900

    build: update build script configuration and dependency installation steps

--- backend/build.sh
+++ backend/build.sh
@@ -3,6 +3,9 @@
 set -o errexit
 
 pip install -r requirements.txt
-prisma py fetch        # ← これを追加！実行バイナリを強制的にダウンロードします
+# 環境変数で指定した場所にバイナリをダウンロード
+prisma py fetch
+# クライアント生成
 prisma generate
+# DB構造の反映
 prisma db push
